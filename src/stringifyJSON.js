67b25bce90a8b92537e4bd25cd51b4dca6b09f7f
Johntejeda <Johntejeda1969@gmail.com>
2018-01-10 10:08:56 -0600

    parse and  stringfy

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,61 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return "null";
+  }
+
+
+  if (obj === undefined || obj.constructor === Function) {
+    return;
+  }
+
+
+  if (obj.constructor === String) {
+    return '"' + obj + '"';
+  }
+
+
+  if (obj.constructor === Array) {
+    if (obj.length) {
+      var partialJSON = [];
+
+      for (var i = 0; i < obj.length; i++) {
+        partialJSON.push(stringifyJSON(obj[i]));
+      }
+
+      return '[' + partialJSON.join(",") + ']';
+    } else {
+      return '[]';
+    }
+  }
+
+
+  if (obj.constructor === Object) {
+    var keys = Object.keys(obj);
+    if (keys.length) {
+      var partialJSON = '';
+
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+
+        if (!key || obj[key] === undefined || typeof key === 'function' || typeof obj[key] === 'function') {
+
+        } else {
+          if (i === keys.length - 1) {
+            partialJSON += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+          } else {
+            partialJSON += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','; //
+          }
+        }
+      }
+      return '{' + partialJSON + '}';
+    } else {
+      return '{}';
+    }
+  }
+
+  // everything else (numbers, booleans, etc.)
+  return obj.toString();
+
 };
